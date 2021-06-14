8167094aeadeb883f5b622a177f6da202f4ca37b
Michael <michaelchristianson93@gmail.com>
2021-06-13 23:39:56 -0700

    fixing mobile responsiveness

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const NotFoundPage = () => {
   return (
     <>
-    <main>
+    <main className="container">
       <title>Not found</title>
       <h1>Page not found</h1>
       <p>

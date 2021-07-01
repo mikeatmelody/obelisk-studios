e01799786f6153b2f5ab11c409d5a05125fa9454
Michael <michaelchristianson93@gmail.com>
2021-07-01 16:09:13 -0700

    brands

--- src/pages/brands.js
+++ src/pages/brands.js
@@ -1,16 +1,33 @@
 import * as React from "react";
-// import { Link } from "gatsby"
 import { motion } from "framer-motion";
 import SEO from "../components/seo";
+// import { graphql } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
+// import BrandsLogo from '../components/BrandsLogos'
+
+// run template query
+// export const query = graphql`
+//   {
+//    allBrands: allContentfulBrands {
+//       nodes {
+//         brandName
+//         logo {
+//           id
+//           gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED, backgroundColor: "#f0eff1", resizingBehavior: FILL)
+//           title
+//         }
+//       }
+//     }
+//   }
+// `;
 
 const BrandsPage = () => {
+  // const brands = allBrands.nodes;
   return (
     <>
       <SEO title="Brands" />
-      <main className="brands container">
+      <main className="container">
         <title>Brands</title>
-        
         <motion.div
           initial={{ opacity: 0, y: -60 }}
           animate={{ opacity: 1, y: 0 }}
@@ -20,57 +37,13 @@ const BrandsPage = () => {
             ease: "easeInOut",
             duration: 1,
           }}
+          className="brands"
         >
+          <StaticImage 
+          src="../images/brand-logos.png" 
+          alt="brand-logos"
+          layout="fullWidth" />
 
-          <div className="brand-titles">
-            <ul className="left-col">
-              <li>ADIDAS</li>
-              <li>GILDAN</li>
-              <li>ALSTYLE</li>
-              <li>ATC</li>
-              <li>AMERICAN APPAREL</li>
-              <li>ANVIL</li>
-              <li>BELLA + CANVAS</li>
-              <li>CALVIN KLEIN</li>
-              <li>COLUMBIA</li>
-              <li>COAL HARBOUR</li>
-              <li>CHAMPION</li>
-              <li>CALLAWAY</li>
-              <li>DICKIES</li>
-              <li>DRY FRAME</li>
-              <li>FLEECE FACTORY</li>
-              <li>FRUIT OF THE LOOM</li>
-              <li>INDEPENDENT</li>
-            </ul>
-            <ul className="right-col">
-              <li>KING ATHLETICS</li>
-              <li>KOI</li>
-              <li>LOS ANGELES APPAREL</li>
-              <li>M&O</li>
-              <li>NEW ERA</li>
-              <li>NORTH FACE</li>
-              <li>NEW BALANCE</li>
-              <li>NIKE</li>
-              <li>OAKLEY</li>
-              <li>PUMA</li>
-              <li>THREADFAST APPAREL</li>
-              <li>VALUCAP</li>
-              <li>Q-TEES</li>
-              <li>VANCOUVER APPAREL</li>
-              <li>FLEXFIT</li>
-              <li>BURNSIDE APPAREL</li>
-              <li>RABBIT SKINS</li>
-            </ul>
-            {/* <div className="catalog-banner">
-            <StaticImage
-              src="../images/pexels-tembela-bohle-1884583.jpg"
-              alt="Catalog Backdrop Image"
-              layout="fullWidth"
-              aspectRatio={21 / 9}
-              transformOptions={{ grayscale: true }}
-            />
-            </div> */}
-          </div>
         </motion.div>
       </main>
     </>
@@ -78,3 +51,16 @@ const BrandsPage = () => {
 };
 
 export default BrandsPage;
+
+
+
+          // {/* {brands.map((brand, index) => (
+          //   <div className="brands-section container" key={index}>
+          //     <GatsbyImage
+          //       key={index}
+          //       image={getImage(brand.logo)}
+          //       alt={brand.logo.title}
+          //     />
+          //     {/* <h1>{brand.brandName}</h1> */}
+          //     </div>
+          //   ))} */}
